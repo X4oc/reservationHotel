cc9f5a2f17e93ca67eaba992eb63697b8301067e
inti0291 <inti0291@DESKTOP-4PAPS7N>
2016-07-11 14:20:16 +0200

    commit exception recherche hotel

---
+++ reservationHotel/src/main/java/com/groupe2/reservationHotel/exceptions/RechercheHotelException.java
@@ -0,0 +1,9 @@
+package com.groupe2.reservationHotel.exceptions;
+
+public class RechercheHotelException extends Exception{
+
+	public RechercheHotelException(String arg0) {
+		super(arg0);
+	}
+
+}
