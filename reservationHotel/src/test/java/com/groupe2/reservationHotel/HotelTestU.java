43012e0e80ec25c60721175680ebdbdcdf9154c7
inti0291 <inti0291@DESKTOP-4PAPS7N>
2016-07-11 15:45:16 +0200

    commit junit hotel vide

---
+++ reservationHotel/src/test/java/com/groupe2/reservationHotel/HotelTestU.java
@@ -0,0 +1,49 @@
+package com.groupe2.reservationHotel;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HotelTestU {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testSetDao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddHotel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteHotel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateHotel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHotels() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHotel() {
+		fail("Not yet implemented");
+	}
+
+}
