25def29d3a9f0096ab57fcca85937d07ba0a93ef
inti0291 <inti0291@DESKTOP-4PAPS7N>
2016-07-13 16:33:45 +0200

    commit hotelTestU et factureTestU

--- reservationHotel/src/test/java/com/groupe2/reservationHotel/FactureTestU.java
+++ reservationHotel/src/test/java/com/groupe2/reservationHotel/FactureTestU.java
@@ -2,11 +2,16 @@ package com.groupe2.reservationHotel;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+import java.util.List;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.groupe2.reservationHotel.entities.Facture;
+import com.groupe2.reservationHotel.entities.Reservation;
 import com.groupe2.reservationHotel.serviceFacture.IFactureService;
 
 public class FactureTestU {
@@ -24,35 +29,109 @@ public class FactureTestU {
 	public static void tearDownAfterClass() throws Exception {
 		context.close();
 	}
+
 /*
 	@Test
-	public void testSetDao() {
-		fail("Not yet implemented");
+	public void testAddFacture() {
+		Facture facture1 = new Facture(new Date(), 1.0);
+		Facture facture2 = new Facture(new Date(), 2.0);
+		Facture facture3 = new Facture(new Date(), 3.0);
+		Facture facture4 = new Facture(new Date(), 4.0);
+		Facture facture5 = new Facture(new Date(), 5.0);
+		Facture facture6 = new Facture(new Date(), 6.0);
+		Facture facture7 = new Facture(new Date(), 7.0);
+		Facture facture8 = new Facture(new Date(), 8.0);
+		Facture facture9 = new Facture(new Date(), 9.0);
+		service.addFacture(facture1);
+		service.addFacture(facture2);
+		service.addFacture(facture3);
+		service.addFacture(facture4);
+		service.addFacture(facture5);
+		service.addFacture(facture6);
+		service.addFacture(facture7);
+		service.addFacture(facture8);
+		service.addFacture(facture9);
+		assertNotNull(facture1.getIdFacture());
+	}
+
+	@Test
+	public void testDeleteFacture() {
+		List<Facture> tabInitial = service.getFactures();
+		service.deleteFacture(3L);
+		List<Facture> tabFinal = service.getFactures();
+		assertNotSame(tabFinal.size(), tabInitial.size());
+	}
+
+	@Test
+	public void testUpdateFacture() throws Exception {
+		Facture f = service.getFacture(4L);
+		f.setTva(50.0);
+		service.updateFacture(f);
+		Facture f2 = service.getFacture(4L);
+		assertEquals(f2.getTva(), f.getTva());
+	}
+
+	@Test
+	public void testGetFacture() throws Exception {
+		Facture f = service.getFacture(5L);
+		assertTrue(f.getIdFacture() == 5L);
+	}
+
+	
+	@Test
+	public void testGetFactures() {
+		List<Facture> tabF = service.getFactures();
+		assertTrue(tabF.size() > 0);
 	}
 */
 	@Test
-	public void testAddFacture() {
-		fail("Not yet implemented");
+	public void tesAddFactureSachantReservation(){
+		Facture facture1 = new Facture(new Date(), 1.0);
+		Facture facture2 = new Facture(new Date(), 2.0);
+		Facture facture3 = new Facture(new Date(), 3.0);
+		Facture facture4 = new Facture(new Date(), 4.0);
+		Facture facture5 = new Facture(new Date(), 5.0);
+		Facture facture6 = new Facture(new Date(), 6.0);
+		Facture facture7 = new Facture(new Date(), 7.0);
+		Facture facture8 = new Facture(new Date(), 8.0);
+		Facture facture9 = new Facture(new Date(), 9.0);
+		
+		service.addFactureSachantReservation(facture1, 1L);
+		service.addFactureSachantReservation(facture2, 1L);
+		service.addFactureSachantReservation(facture3, 1L);
+		service.addFactureSachantReservation(facture4, 1L);
+		service.addFactureSachantReservation(facture5, 1L);
+		service.addFactureSachantReservation(facture6, 1L);
+		service.addFactureSachantReservation(facture7, 1L);
+		service.addFactureSachantReservation(facture8, 1L);
+		service.addFactureSachantReservation(facture9, 1L);
+		assertNotNull(facture1.getIdFacture());		
 	}
-/*
+/*	
 	@Test
-	public void testDeleteFacture() {
-		fail("Not yet implemented");
+	public void testGetFacturesByClient(){
+		List<Facture> tabF = service.getFacturesByClient(5L);
+		assertTrue(tabF.size() > 0);
+	}
+	
+	@Test
+	public void testCalculerCoutConsommationParClient(){
+		assertTrue(service.calculerCoutConsommationParClient(1L) > 0);
+		
+	}
+	
+	@Test
+	public void testCalculerCoutTotalParClient(){
+		assertTrue(service.calculerCoutTotalParClient(1L) > 0);
+	}
+	
+		@Test void testCalculerCoutTotalAnnuelParClient(){
+		assertTrue(service.calculerCoutTotalAnnuelParClient(1L) > 0);
+	}
+	
+	@Test void testCalculerCoutTotalSemestrielParClient(){
+		assertTrue(service.calculerCoutTotalSemestrielParClient(1L) > 0);
 	}
 
-	@Test
-	public void testUpdateFacture() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetFacture() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetFactures() {
-		fail("Not yet implemented");
-	}
 */
 }
--- reservationHotel/src/test/java/com/groupe2/reservationHotel/HotelTestU.java
+++ reservationHotel/src/test/java/com/groupe2/reservationHotel/HotelTestU.java
@@ -2,11 +2,15 @@ package com.groupe2.reservationHotel;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.groupe2.reservationHotel.entities.Client;
+import com.groupe2.reservationHotel.entities.Employe;
 import com.groupe2.reservationHotel.entities.Hotel;
 import com.groupe2.reservationHotel.serviceHotel.IHotelService;
 
@@ -25,37 +29,69 @@ public class HotelTestU {
 	public static void tearDownAfterClass() throws Exception {
 		context.close();
 	}
-/*
-	@Test
-	public void testSetDao() {
-		fail("Not yet implemented");
-	}
-*/
+
 	@Test
 	public void testAddHotel() {
 		Hotel hotel1 = new Hotel("nomHotel1", "adrHotel1", 2, "numeroTelephone1", "numeroFax1", "codePostal1", "pays1", "adresseMail1");
+		Hotel hotel2 = new Hotel("nomHotel2", "adrHotel2", 4, "numeroTelephone2", "numeroFax2", "codePostal2", "pays2", "adresseMail2");
+		Hotel hotel3 = new Hotel("nomHotel3", "adrHotel3", 6, "numeroTelephone3", "numeroFax3", "codePostal3", "pays3", "adresseMail3");
+		Hotel hotel4 = new Hotel("nomHotel4", "adrHotel4", 8, "numeroTelephone4", "numeroFax4", "codePostal4", "pays4", "adresseMail4");
+		Hotel hotel5 = new Hotel("nomHotel5", "adrHotel5", 10, "numeroTelephone5", "numeroFax5", "codePostal5", "pays5", "adresseMail5");
+		Hotel hotel6 = new Hotel("nomHotel6", "adrHotel6", 12, "numeroTelephone6", "numeroFax6", "codePostal6", "pays6", "adresseMail6");
+		Hotel hotel7 = new Hotel("nomHotel7", "adrHotel7", 14, "numeroTelephone7", "numeroFax7", "codePostal7", "pays7", "adresseMail7");
+		Hotel hotel8 = new Hotel("nomHotel8", "adrHotel8", 16, "numeroTelephone8", "numeroFax8", "codePostal8", "pays8", "adresseMail8");
+		Hotel hotel9 = new Hotel("nomHotel9", "adrHotel9", 18, "numeroTelephone9", "numeroFax9", "codePostal9", "pays9", "adresseMail9");
 		service.addHotel(hotel1);
+		service.addHotel(hotel2);
+		service.addHotel(hotel3);
+		service.addHotel(hotel4);
+		service.addHotel(hotel5);
+		service.addHotel(hotel6);
+		service.addHotel(hotel7);
+		service.addHotel(hotel8);
+		service.addHotel(hotel9);
 		assertNotNull(hotel1.getIdHotel());
-	}
-/*
-	@Test
-	public void testDeleteHotel() {
-		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testUpdateHotel() {
-		fail("Not yet implemented");
+	public void testDeleteHotel() {
+		List<Hotel> tabInitial = service.getHotels();
+		service.deleteHotel(3L);
+		List<Hotel> tabFinal = service.getHotels();
+		assertNotSame(tabFinal.size(), tabInitial.size());
+	}
+
+	@Test
+	public void testUpdateHotel() throws Exception {
+		Hotel h = service.getHotel(7L);
+		h.setNomHotel("nom modifie 7");
+		service.updateHotel(h);
+		Hotel h2 = service.getHotel(7L);
+		assertEquals(h2.getNomHotel(), h.getNomHotel());
 	}
 
 	@Test
 	public void testGetHotels() {
-		fail("Not yet implemented");
+		List<Hotel> tabH = service.getHotels();
+		assertTrue(tabH.size() > 0);
 	}
 
 	@Test
-	public void testGetHotel() {
-		fail("Not yet implemented");
+	public void testGetHotel() throws Exception {
+		Hotel h = service.getHotel(5L);
+		assertTrue(h.getIdHotel() == 5L);
 	}
-*/
+
+	@Test
+	public void testgetEmployesHotel(){
+		List<Employe> tabE = service.getEmployesHotel(1L);
+		assertTrue(tabE.size() > 0);
+	}
+	
+	@Test
+	public void testgetClientsHotel(){
+		List<Client> tabC = service.getClientsHotel(1L);
+		assertTrue(tabC.size() > 0);
+	}
+	
 }
